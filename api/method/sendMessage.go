d56f85603c629adc4cb46e7190040a4bf15beb4d
wildwind <z-petr-i@mail.ru>
2020-10-11 18:49:57 +1000

    - package name

--- api/method/sendMessage.go
+++ api/method/sendMessage.go
@@ -3,13 +3,13 @@ package method
 
 import (
 	"encoding/json"
-	"telegram_api/api/model"
+	"github.com/wildwind123/telegram_api/api/model"
 )
 
 type SendMessage string
 
 type SendMessageResponseStruct struct {
-	Ok bool `json:"ok"`
+	Ok     bool `json:"ok"`
 	Result model.Message
 }
 
@@ -19,19 +19,19 @@ type sendMessageParams struct {
 	DisableNotification *bool  `json:"disable_notification,omitempty"`
 }
 
-func (method *SendMessage) GetParams(ChatId, Text string) (*sendMessageParams, error){
+func (method *SendMessage) GetParams(ChatId, Text string) (*sendMessageParams, error) {
 
 	if ChatId == "" {
-		return nil, ErrorMsg{Msg:"Chat is required"}
+		return nil, ErrorMsg{Msg: "Chat is required"}
 	}
 
 	if Text == "" {
-		return nil, ErrorMsg{Msg:"Text is required"}
+		return nil, ErrorMsg{Msg: "Text is required"}
 	}
 
 	sm := &sendMessageParams{
-		ChatId:              ChatId,
-		Text:                Text,
+		ChatId: ChatId,
+		Text:   Text,
 	}
 
 	return sm, nil
@@ -57,6 +57,3 @@ func (method *sendMessageParams) Request() (*SendMessageResponseStruct, error) {
 	}
 	return &respStr, nil
 }
-
-
-
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module telegram_api
+module github.com/wildwind123/telegram_api
 
 go 1.15
